5de6eef50748274a5213f6593714887888fe3691
ardaboluk <ardaboluk2@gmail.com>
2019-10-18 11:05:38 +0300

    Added sampson error to getFMatInlierIndices which is used in estimateFundamentalMatrixRANSAC.

--- rectification.cpp
+++ rectification.cpp
@@ -30,25 +30,36 @@ std::vector<int> Rectification::getFMatInlierIndices(std::vector<cv::Point2f> co
         point2Mat.at<double>(1,0) = (double)correspondingPoints2[i].y;
         point2Mat.at<double>(2,0) = 1.0;
 
+		cv::Mat resultMat = point2Mat.t() * fundamentalMatrix * point1Mat;
+		double epipolarConstraintScore = resultMat.at<double>(0,0);
+
 		cv::Mat line2Mat = fundamentalMatrix * point1Mat;
 		cv::Mat line1Mat = fundamentalMatrix.t() * point2Mat;
-		double a1 = line1Mat.at<double>(0,0);
-		double b1 = line1Mat.at<double>(1,0);
-		double c1 = line1Mat.at<double>(2,0);
-		double a2 = line2Mat.at<double>(0,0);
-		double b2 = line2Mat.at<double>(1,0);
-		double c2 = line2Mat.at<double>(2,0);
-		double x1 = point1Mat.at<double>(0,0);
-		double y1 = point1Mat.at<double>(1,0);
-		double x2 = point2Mat.at<double>(0,0);
-		double y2 = point2Mat.at<double>(1,0);
 
-		double distP1L2 = abs(a2*x1 + b2*y1 + c2) / sqrt(a2*a2 + b2*b2);
-		double distP2L1 = abs(a1*x2 + b1*y2 + c1) / sqrt(a1*a1 + b1*b1);
-
-		if(distP2L1 <= thr && distP1L2 <= thr){
+		double sampsonErrorNum = pow(epipolarConstraintScore, 2.0);
+		double sampsonErrorDenom = pow(line2Mat.at<double>(0,0), 2.0) + pow(line2Mat.at<double>(1,0), 2.0) + pow(line1Mat.at<double>(0,0), 2.0) + pow(line1Mat.at<double>(1,0), 2.0);
+		double sampsonError = sampsonErrorNum / sampsonErrorDenom;
+		if(sampsonError <= thr){
 			inlierIndices.push_back(i);
 		}
+
+		// double a1 = line1Mat.at<double>(0,0);
+		// double b1 = line1Mat.at<double>(1,0);
+		// double c1 = line1Mat.at<double>(2,0);
+		// double a2 = line2Mat.at<double>(0,0);
+		// double b2 = line2Mat.at<double>(1,0);
+		// double c2 = line2Mat.at<double>(2,0);
+		// double x1 = point1Mat.at<double>(0,0);
+		// double y1 = point1Mat.at<double>(1,0);
+		// double x2 = point2Mat.at<double>(0,0);
+		// double y2 = point2Mat.at<double>(1,0);
+
+		// double distP1L2 = abs(a2*x1 + b2*y1 + c2) / sqrt(a2*a2 + b2*b2);
+		// double distP2L1 = abs(a1*x2 + b1*y2 + c1) / sqrt(a1*a1 + b1*b1);
+
+		// if(distP2L1 <= thr && distP1L2 <= thr){
+		// 	inlierIndices.push_back(i);
+		// }
 
 		// cv::Mat resultMat = point2Mat.t() * fundamentalMatrix * point1Mat;
 		// double score = resultMat.at<double>(0,0);
@@ -172,7 +183,7 @@ std::pair<cv::Mat, cv::Mat> Rectification::rectifyImages(bool use_ransac){
 	cv::Mat fundamentalMatrix;
 	cv::Mat fundamentalMatrixDenormalized;
 	if(use_ransac){
-		fundamentalMatrixDenormalized = estimateFundamentalMatrixRANSAC(correspondingPoints1, correspondingPoints2, 3.0, 1000);
+		fundamentalMatrixDenormalized = estimateFundamentalMatrixRANSAC(correspondingPoints1, correspondingPoints2, 3.0, 5000);
 		Util::displayMat(fundamentalMatrixDenormalized, "RANSAC Fundamental Matrix");
 	}else{
 		fundamentalMatrix = estimator.estimateFundamentalMatrix8Point(correspondingPoints1_normalized, correspondingPoints2_normalized);
